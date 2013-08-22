14742b3fa8a2b94aa0b23a94927603210c9246a4
Johan Broström <Job76@users.noreply.github.com>
2013-08-22 17:48:17 +0200

    Update About.ino

--- Controller/About.ino
+++ Controller/About.ino
@@ -7,7 +7,7 @@ Version             : 0.50
 Started             : 2013-05-21
 Updated             : 2013-05-28
 Author              : Johan Broström
-E-Mail              : johan.brostrom@inspektionssystem.se
+E-Mail              : johan.brostrom(at)inspektionssystem.se
 Company             : InspektionsSystem AB
 Homepage            : www.inspektionssystem.eu
 
